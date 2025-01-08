68e8d4713fe382189ca4251a9dc718e0ecf06087
Jeremy Ward <jeremy@jmichaelward.com>
2025-01-07 20:58:28 -0600

    Initialize database state.

--- modules/php/Game.php
+++ modules/php/Game.php
@@ -276,6 +276,55 @@ class Game extends \Table
         // $this->initStat("player", "player_teststat1", 0);
 
         // TODO: Setup the initial game situation here.
+        $sql = "INSERT INTO stones (`player`, `board`, `square`) VALUES ";
+
+        $stonesSetup = [
+          // Player One
+          [1, 1, 13],
+          [1, 1, 14],
+          [1, 1, 15],
+          [1, 1, 16],
+          [1, 2, 13],
+          [1, 2, 14],
+          [1, 2, 15],
+          [1, 2, 16],
+          [1, 3, 13],
+          [1, 3, 14],
+          [1, 3, 15],
+          [1, 3, 16],
+          [1, 4, 13],
+          [1, 4, 14],
+          [1, 4, 15],
+          [1, 4, 16],
+
+          // Player Two
+          [2, 1, 1],
+          [2, 1, 2],
+          [2, 1, 3],
+          [2, 1, 4],
+          [2, 2, 1],
+          [2, 2, 2],
+          [2, 2, 3],
+          [2, 2, 4],
+          [2, 3, 1],
+          [2, 3, 2],
+          [2, 3, 3],
+          [2, 3, 4],
+          [2, 4, 1],
+          [2, 4, 2],
+          [2, 4, 3],
+          [2, 4, 4],
+        ];
+
+        $insertValues = [];
+
+        foreach ($stonesSetup as $stone) {
+          $insertValues[] = "({$stone[0]}, {$stone[1]}, {$stone[2]})";
+        }
+
+        $sql .= implode(',', $insertValues);
+
+        $this->dbQuery($sql);
 
         // Activate first player once everything has been initialized and ready.
         $this->activeNextPlayer();
